f4c5b1ed5d56cb5b2d05961b8015f84c72f038b6
AEMuto <antoine.marseaud@gmail.com>
2022-07-12 09:52:00 +0200

    Try to make it works on Heroku.

--- app/index.js
+++ app/index.js
@@ -1,12 +1,22 @@
 const express = require('express')
+const dotEnv = require('dotenv')
 const cors = require('cors')
 
+dotEnv.config()
 const router = require('./routes')
 
 const app = express()
+const PORT = process.env.PORT || 3001
+
 app.use(cors())
-const port = 3000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.use(router)
 
-app.listen(port, () => console.log(`Magic happens on port ${port}`))
+app.get('/', (req, res, next) => {
+	res.send('Hello from my Express server v2!')
+})
+
+app.listen(PORT, () => console.log(`Server listening on http://localhost:${PORT}`))
